4a617d40e46ceb4cd916cc4288f2545881ce9d93
Vitalik <burbanvitalik2002@gmail.com>
2025-06-23 20:04:22 +0300

    add config & graphql

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,25 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { GraphQLModule } from "@nestjs/graphql";
+import { ApolloDriver } from "@nestjs/apollo";
+import { join } from "path";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [".env"], // ми це робимо, щоб вказати шлях до файлу з змінними середовища
+      isGlobal: true, // робимо змінні середовища доступними глобально
+    }),
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), "src/schema.gql"),
+      sortSchema: true,
+      playground: true,
+      context: ({ req }) => ({ headers: req.headers }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ConfigService } from "@nestjs/config";
+
+const DEFAULT_PORT = 4200;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(ConfigService);
+  const portFromConfig = configService.get<number>("PORT") || DEFAULT_PORT;
+  const finalPort = process.env.PORT
+    ? parseInt(process.env.PORT, 10)
+    : portFromConfig;
+
+  await app.listen(finalPort);
 }
+
 bootstrap();
